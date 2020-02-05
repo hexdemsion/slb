5c10ed67e8d2310dfc30aa1f9565e63291e885cf
Ahmad Malhadi <hexdemsion@gmail.com>
2020-02-06 00:36:23 +0800

    doing function in object to call linked formula

--- form.js
+++ form.js
@@ -1,5 +1,3 @@
-// console.log(DATASET)
-
 function renderTool() {
 	for (var i = 0; i < DATASET.tool.length; i++) {
 		var option = document.createElement("option")
@@ -29,15 +27,50 @@ function renderHole(view) {
 	}
 }
 
-
-function doCalculate() {
-	document.getElementById("form_wrapper").style.display = "none"
-	document.getElementById("result_wrapper").style.display = "block"
-}
-
-
 function backAction() {
 	document.getElementById("calc_form").reset()
 	document.getElementById("form_wrapper").style.display = "block"
 	document.getElementById("result_wrapper").style.display = "none"
 }
+
+function doCalculate() {
+	// document.getElementById("form_wrapper").style.display = "none"
+	document.getElementById("result_wrapper").style.display = "block"
+
+	var req = {
+	    "id_tool": null,
+	    "hole": {
+	    	"uphole": {"conn": null, "rop2usc": null, "crossover": null},
+	    	"downhole": {"conn": null, "rop2usc": null, "crossover": null}
+	    }
+	}	
+
+	req.id_tool = parseInt(document.getElementById("tool_selector").value)
+
+	req.hole.uphole.conn = document.getElementById("up_connection").value
+	req.hole.uphole.rop2usc = parseInt(document.getElementById("up_rop2usc").value)
+	req.hole.uphole.crossover = parseInt(document.getElementById("up_crossover").value)
+
+	req.hole.downhole.conn = document.getElementById("down_connection").value
+	req.hole.downhole.rop2usc = parseInt(document.getElementById("down_rop2usc").value)
+	req.hole.downhole.crossover = parseInt(document.getElementById("down_crossover").value)
+
+	if (req.hole.uphole.conn == "pin") {
+		var res_uphole = DATASET.tool.find(el => el.id === req.id_tool).uphole.pin(req.hole.uphole.rop2usc, req.hole.uphole.crossover)
+	}else if (req.hole.uphole.conn == "box") {
+		var res_uphole = DATASET.tool.find(el => el.id === req.id_tool).uphole.box(req.hole.uphole.rop2usc, req.hole.uphole.crossover)
+	}
+
+	if (req.hole.downhole.conn == "pin") {
+		var res_downhole = DATASET.tool.find(el => el.id === req.id_tool).downhole.pin(req.hole.downhole.rop2usc, req.hole.downhole.crossover)
+	}else if (req.hole.downhole.conn == "box") {
+		var res_downhole = DATASET.tool.find(el => el.id === req.id_tool).downhole.box(req.hole.downhole.rop2usc, req.hole.downhole.crossover)
+	}
+
+	console.log(res_uphole, res_downhole)
+
+}
+
+
+// console.log(ecoscope_uphole_box(233, 14.5))
+// console.log(optidrill900_downhole_pin(45, 14.5))
---
+++ object.js
@@ -0,0 +1,347 @@
+var DATASET = {
+    "extender": {
+        "type": {
+            "uphole": [
+            	{"id": 1, "name": "MEXM-BA", "min": 28.13, "max": 31.65, "tolerance": 0}, 
+            	{"id": 2, "name": "MEXM-AA", "min": 31.63, "max": 35.15, "tolerance": 0}, 
+            	{"id": 3, "name": "MEXM-DA", "min": 34.13, "max": 37.65, "tolerance": 0}, 
+
+            	{"id": 4, "name": "EXTM-CB", "min": 11.25, "max": 13.75, "tolerance": 0}, 
+            	{"id": 5, "name": "EXTM-BB", "min": 13.37, "max": 15.87, "tolerance": 0}, 
+            	{"id": 6, "name": "EXTM-FA", "min": 15.49, "max": 17.99, "tolerance": 0}, 
+            	{"id": 7, "name": "EXTM-GB", "min": 17.58, "max": 20.14, "tolerance": 0}, 
+            	
+            	{"id": 8, "name": "RTLM-CC", "min": 29.54, "max": 32.28, "tolerance": 0}, 
+            	{"id": 9, "name": "RTLM-BD", "min": 39.28, "max": 42.02, "tolerance": 0}
+            ],
+            "downhole": [
+            	{"id": 1, "name": "EXTF-AA", "min": 12.82, "max": 15.16, "tolerance": 0}, 
+            	{"id": 2, "name": "EXTF-BA", "min": 10.41, "max": 12.75, "tolerance": 0}, 
+            	{"id": 3, "name": "EXTF-CA", "min": 8.06, "max": 10.40, "tolerance": 0}, 
+            	{"id": 4, "name": "EXTF-DA", "min": 12.82, "max": 15.16, "tolerance": 0}, 
+            	{"id": 5, "name": "EXTF-DB", "min": 12.82, "max": 15.16, "tolerance": 0}, 
+            	
+            	{"id": 6, "name": "MEXD-GA", "min": 22.36, "max": 24.36, "tolerance": 26.86}, 
+            	{"id": 7, "name": "MEXD-DB", "min": 25.36, "max": 27.76, "tolerance": 30.26}, 
+            	{"id": 8, "name": "MEXD-BA", "min": 27.82, "max": 30.16, "tolerance": 32.66}, 
+            	{"id": 9, "name": "MEXD-CB", "min": 30.22, "max": 32.56, "tolerance": 35.06}, 
+            	{"id": 10, "name": "MEXD-EB", "min": 31.96, "max": 34.36, "tolerance": 36.86}, 
+            	
+            	{"id": 11, "name": "MXLD-DA", "min": 49.39, "max": 51.73, "tolerance": 54.23}, 
+            	{"id": 12, "name": "MXLD-BA", "min": 51.82, "max": 54.16, "tolerance": 56.66}, 
+            	{"id": 13, "name": "MXLD-G/BA", "min": 51.82, "max": 54.16, "tolerance": 56.66}, 
+            	
+            	{"id": 14, "name": "RTLF-CA", "min": 12.85, "max": 13.75, "tolerance": 0}, 
+            	{"id": 15, "name": "RTLF-BB", "min": 14.96, "max": 17.58, "tolerance": 0}, 
+            	{"id": 16, "name": "RTLF-EA", "min": 28.81, "max": 31.43, "tolerance": 0}
+            ]
+        }
+    },
+    "tool": [
+        {
+            "id": 1,
+            "name": "ADN 4",
+            "view": 2,
+            "id_uphole": [],
+            "id_downhole": [14,15,16],
+            "downhole": {
+                "box": function(rop2usc, crossover){ return adn4_downhole_box(rop2usc, crossover) },
+            }
+        },
+        {
+            "id": 2,
+            "name": "ARC 6",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return arc6_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return arc6_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return arc6_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return arc6_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 3,
+            "name": "ARC 8",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return arc8_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return arc8_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return arc8_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return arc8_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 4,
+            "name": "ARC 9",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return arc9_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return arc9_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return arc9_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return arc9_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 5,
+            "name": "Ecoscope",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return ecoscope_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return ecoscope_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return ecoscope_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return ecoscope_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 6,
+            "name": "Geosphere Transmitter",
+            "view": 0,
+            "id_uphole": [8],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return geosphere_transmitter_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc, crossover){ return geosphere_transmitter_downhole_box(rop2usc, crossover) },
+            }
+        },
+        {
+            "id": 7,
+            "name": "Geosphere Receiver",
+            "view": 0,
+            "id_uphole": [8],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return geosphere_receiver_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc, crossover){ return geosphere_receiver_downhole_box(rop2usc, crossover) },
+            }
+        },
+        {
+            "id": 8,
+            "name": "Gyropulse",
+            "view": 0,
+            "id_uphole": [1,2,3],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10,13],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return gyropulse_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return gyropulse_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return gyropulse_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return gyropulse_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 9,
+            "name": "Impulse",
+            "view": 1,
+            "id_uphole": [9],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return impulse_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc, crossover){ return impulse_downhole_box(rop2usc, crossover) },
+            }
+        },
+        {
+            "id": 10,
+            "name": "Microscope 475",
+            "view": 0,
+            "id_uphole": [8],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return microscope475_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return microscope475_downhole_box(rop2usc) },
+            }
+        },
+        {
+            "id": 11,
+            "name": "Microscope 675",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return microscope675_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return microscope675_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return microscope675_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return microscope675_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 12,
+            "name": "Optidrill 675",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc){ return optidrill675_uphole_box(rop2usc) },
+            },
+            "downhole": {
+                "pin": function(rop2usc, crossover){ return optidrill675_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 13,
+            "name": "Optidrill 900",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return optidrill900_uphole_box(rop2usc, crossover) },
+            },
+            "downhole": {
+                "pin": function(rop2usc, crossover){ return optidrill900_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 14,
+            "name": "Periscope 675",
+            "view": 0,
+            "id_uphole": [4,5,6,7],
+            "id_downhole": [6,7,8,9,10]
+        },
+        {
+            "id": 15,
+            "name": "Periscope 475",
+            "view": 0,
+            "id_uphole": [8],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return periscope475_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return periscope475_downhole_box(rop2usc) },
+            }
+        },
+        {
+            "id": 16,
+            "name": "Sonicscope 675",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return sonicscope_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return sonicscope_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return sonicscope_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return sonicscope_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 17,
+            "name": "Telescope IWOB",
+            "view": 0,
+            "id_uphole": [1,2,3],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10,13],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return telescope_iwob_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return telescope_iwob_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return telescope_iwob_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return telescope_iwob_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 18,
+            "name": "Telescope Non IWOB",
+            "view": 0,
+            "id_uphole": [1,2,3],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10,13],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return telescope_non_iwob_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return telescope_non_iwob_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return telescope_non_iwob_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return telescope_non_iwob_downhole_pin(rop2usc, crossover) }
+            }
+        },
+        {
+            "id": 19,
+            "name": "VPWD",
+            "view": 0,
+            "id_uphole": [8],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return vpwd_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return vpwd_downhole_box(rop2usc) },
+            }
+        },
+        {
+            "id": 20,
+            "name": "Sonicvision",
+            "view": 0,
+            "id_uphole": [1,2,3],
+            "id_downhole": [11,12]
+        },
+        {
+            "id": 21,
+            "name": "Digiscope",
+            "view": 0,
+            "id_uphole": [9],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "pin": function(rop2usc, crossover){ return digiscope_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc, crossover){ return digiscope_downhole_box(rop2usc, crossover) },
+            }
+        },
+        {
+            "id": 22,
+            "name": "Stethoscope 4",
+            "view": 0,
+            "id_uphole": [8],
+            "id_downhole": [14,15,16],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return stethoscope4_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return stethoscope4_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return stethoscope4_downhole_box(rop2usc) },
+            }
+        },
+        {
+            "id": 23,
+            "name": "Stethoscope 6",
+            "view": 0,
+            "id_uphole": [1,2,3,4,5,6,7],
+            "id_downhole": [1,2,3,4,5,6,7,8,9,10],
+            "uphole": {
+                "box": function(rop2usc, crossover){ return stethoscope6_uphole_box(rop2usc, crossover) },
+                "pin": function(rop2usc, crossover){ return stethoscope6_uphole_pin(rop2usc, crossover) }
+            },
+            "downhole": {
+                "box": function(rop2usc){ return stethoscope6_downhole_box(rop2usc) },
+                "pin": function(rop2usc, crossover){ return stethoscope6_downhole_pin(rop2usc, crossover) }
+            }
+        }
+    ]
+}
